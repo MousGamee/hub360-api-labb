429eca61f573a3fd8a1907123269de614e3c0a3f
MousGamee <moussa.sagna@hotmail.fr>
2023-07-17 15:22:30 +0200

    modules end

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,27 +8,27 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
+  createUser(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.createUser(createUserDto);
   }
 
   @Get()
   findAll() {
-    return this.usersService.findAll();
+    return this.usersService.findAllUsers();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
+  findOneUser(@Param('id') id: string) {
+    return this.usersService.findOneUser(+id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
+  updateUser(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.usersService.updateUser(+id, updateUserDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
+  removeUser(@Param('id') id: string) {
+    return this.usersService.removeUser(+id);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,35 +2,47 @@ import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
-const model = {
-  _id: '123aze',
-  id: 'b',
-  lastname: 'Robinson',
-  firstname: 'Nick',
-  role: 'manager-co',
-  _deleted: false,
-  updatedAt: '2023-07-17T06:36:35.161Z',
-};
+const users = [
+  {
+    _id: '123aze',
+    id: 'b',
+    lastname: 'Robinson',
+    firstname: 'Nick',
+    role: 'manager-co',
+    _deleted: false,
+    updatedAt: '2023-07-17T06:36:35.161Z',
+  },
+
+  {
+    _id: '64b4e00f575cd882f44f991c',
+    id: 'c',
+    lastname: 'Qualley',
+    firstname: 'Margaret',
+    role: 'admin',
+    _deleted: false,
+    updatedAt: '2023-07-17T06:51:43.161Z',
+  },
+];
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  createUser(createUserDto: CreateUserDto) {
+    return 'create a user';
   }
 
-  findAll() {
-    return `This action returns all users`;
+  findAllUsers() {
+    return users;
   }
 
-  findOne(id: number) {
+  findOneUser(id: number) {
     return `This action returns a #${id} user`;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
+  updateUser(id: number, updateUserDto: UpdateUserDto) {
     return `This action updates a #${id} user`;
   }
 
-  remove(id: number) {
+  removeUser(id: number) {
     return `This action removes a #${id} user`;
   }
 }
